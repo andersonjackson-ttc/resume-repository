6bb405c93977a86cf31e7aa11a83b3b3f2cd5229
khalidsmalls <53487854+khalidsmalls@users.noreply.github.com>
2020-09-30 01:46:44 -0400

    landing page
    created header, footer, and index.html with basic bootstap style

---
+++ WS/www/index.php
@@ -0,0 +1,23 @@
+<?php
+$page_title = 'Resume Repository Home';
+include('../includes/header.html');
+?>
+
+<div class="page-header"><h1>Content Header</h1></div>
+<hr />
+
+<p>A singulis tractavissent ad multos in ea varias offendit. Eiusmod distinguantur
+qui ingeniis eu ea magna noster quorum mandaremus, probant quorum lorem an enim
+qui eu multos eiusmod mentitum aut ita eram concursionibus ex excepteur quis quo
+officia familiaritatem, illum aliquip si singulis, sed elit concursionibus. Te
+quem ab illum. Nescius ex quamquam. Si aute probant philosophari do ne se labore
+dolore dolor o ingeniis labore fore an fugiat, in amet nostrud. Duis do
+senserit, occaecat labore officia, cernantur malis mandaremus pariatur ita velit
+arbitror se transferrem. Aut fabulas ad aliquip, do quorum eu enim ne aut ut
+minim nostrud, dolor nescius an exercitation, litteris nulla aut quamquam
+exquisitaque, esse cupidatat an ullamco si ullamco hic quem admodum, ubi tamen
+summis an probant.</p>
+
+<?php
+include('../includes/footer.html');
+?>
