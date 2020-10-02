8a7d77d98ac3f2c5f69dee852a17f363eb47c109
JBihlear <71603063+JBihlear@users.noreply.github.com>
2020-10-01 23:15:31 -0400

    Added Various Checkboxes
    + Added Technical Skills Checkboxes
    + Added Professional Skills Checkboxes
    + Added Military Veteran Checkbox
    + Added Security Clearance Checkbox

--- WS/www/newstudentform.php
+++ WS/www/newstudentform.php
@@ -64,14 +64,113 @@
 				<div id="inputField">
 					<label>Phone Number *<br><input required name="studentPhone" type="phone" size="30" maxlength="100" value="<?php if (isset($_POST['studentPhone'])) echo $_POST['studentPhone']; ?>"></label>
 				</div>
+				<div id="checkbox">
+					<label>Military Veteran *<br><input name="militaryStatus" type="checkbox" value="<?php if (isset($_POST['militaryStatus'])) echo $_POST['militaryStatus']; ?>"></label>
+					<br>
+					<label>Security Clearance *<br><input name="securityClearance" type="checkbox" value="<?php if (isset($_POST['securityClearance'])) echo $_POST['securityClearance']; ?>"></label>
+				</div>
 				<!--End first row-->
 				<br>
 				<!--Begin second row-->
 				<div id="inputField">
 					<label>Technical Skills<br><textarea class="skillsBox" name="techSkills" type="text"><?php if (isset($_POST['techSkills'])) echo $_POST['techSkills']; ?></textarea></label>
+					<!-- Start of Technical Skills Checkboxes -->
+					<ul style="column-count: 2; list-style-type: none;">
+						<li>
+							<input type="checkbox" id="skillProgram" name="skillProgram" value="skillProgram">
+							<label for="skillProgram">Programming Languages (1 or more)</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillProject" name="skillProject" value="skillProject">
+							<label for="skillProject">Project Management</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillData" name="skillData" value="skillData">
+							<label for="skillData">Data Analysis</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillWriting" name="skillWriting" value="skillWriting">
+							<label for="skillWriting">Technical Writing</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillLogistics" name="skillLogistics" value="skillLogistics">
+							<label for="skillLogistics">Logistics Management</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillAdobe" name="skillAdobe" value="skillAdobe">
+							<label for="skillAdobe">Adobe Software</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillCad" name="skillCad" value="skillCad">
+							<label for="skillCad">CAD Software</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillBookkeeping" name="skillBookkeeping" value="skillBookkeeping">
+							<label for="skillBookkeeping">Bookkeeping Software</label>
+						</li>
+					</ul> <!-- End of Technical Skills Checkboxes -->
 				</div>
 				<div id="inputField">
 					<label>Professional Skills<br><textarea class="skillsBox" name="profSkills" type="text"><?php if (isset($_POST['profSkills'])) echo $_POST['profSkills']; ?></textarea></label>
+					<!-- Start of Professional Skills Checkboxes -->
+					<ul style="column-count: 2; list-style-type: none;">
+						<li>
+							<input type="checkbox" id="skillCritThinking" name="skillCritThinking" value="skillCritThinking">
+							<label for="skillCritThinking">Critical Thinking and Problem Solving</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillInterpersonal" name="skillInterpersonal" value="skillInterpersonal">
+							<label for="skillInterpersonal">Interpersonal Skills</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillCommunication" name="skillCommunication" value="skillCommunication">
+							<label for="skillCommunication">Communication Ability</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillAttitude" name="skillAttitude" value="skillAttitude">
+							<label for="skillAttitude">Professional Attitude and Demeanor</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillAlertness" name="skillAlertness" value="skillAlertness">
+							<label for="skillAlertness">Alertness, Ability to Focus</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillAdaptability" name="skillAdaptability" value="skillAdaptability">
+							<label for="skillAdaptability">Agility and Adaptability</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillResearching" name="skillResearching" value="skillResearching">
+							<label for="skillResearching">Researching Information</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillPersMan" name="skillPersMan" value="skillPersMan">
+							<label for="skillPersMan">Personal Management</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillCreativity" name="skillCreativity" value="skillCreativity">
+							<label for="skillCreativity">Creativity and Innovation</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillLiteracy" name="skillLiteracy" value="skillLiteracy">
+							<label for="skillLiteracy">Literacy</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillLeadership" name="skillLeadership" value="skillLeadership">
+							<label for="skillLeadership">Leadership</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillStressMgmt" name="skillStressMgmt" value="skillStressMgmt">
+							<label for="skillStressMgmt">Stress Management</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillStudy" name="skillStudy" value="skillStudy">
+							<label for="skillStudy">Study Skills</label>
+						</li>
+						<li>
+							<input type="checkbox" id="skillHonesty" name="skillHonesty" value="skillHonesty">
+							<label for="skillHonesty">Honesty and Integrity</label>
+						</li>
+					</ul> <!-- End of Professional Skills Checkboxes -->
 				</div>
 				<!--End second row-->
 				<br>
