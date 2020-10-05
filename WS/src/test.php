478973e54136d9b1544a95874322fe0cb25d320b
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-05 09:45:01 -0400

    Create test.php

---
+++ WS/src/test.php
@@ -0,0 +1,26 @@
+
+
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "charlestoncarts";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+if (!$conn) 
+{
+die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+
+ $first = $_POST['first'];
+ $last = $_POST['last'];
+ $email = $_POST['email'];
+ $phone = $_POST['phone'];
+ 
+
+ $sql = "INSERT INTO Students (first_name, last_name, email, phone) VALUES ('$first', '$last', '$email', '$phone');";
+
+ mysqli_query($conn, $sql);
