bd5a78727a3d4d685953de26e61a2ba98eae7ff4
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-06 20:25:58 -0400

    Update index.php

--- WS/www/index.php
+++ WS/www/index.php
@@ -8,25 +8,13 @@ include('../includes/header.html');
 <div class="page-header"><h1>Resume Repository</h1></div>
 <hr />
 
-<button>Profiles List</button>
-<button>New Student Profile</button>
-<button>Certification List</button>
-<button>Majors List</button>
-<button>Profiles List</button>
-<button>Profiles List</button>
-<button>Employment List</button>
 
-<p>A singulis tractavissent ad multos in ea varias offendit. Eiusmod distinguantur
-qui ingeniis eu ea magna noster quorum mandaremus, probant quorum lorem an enim
-qui eu multos eiusmod mentitum aut ita eram concursionibus ex excepteur quis quo
-officia familiaritatem, illum aliquip si singulis, sed elit concursionibus. Te
-quem ab illum. Nescius ex quamquam. Si aute probant philosophari do ne se labore
-dolore dolor o ingeniis labore fore an fugiat, in amet nostrud. Duis do
-senserit, occaecat labore officia, cernantur malis mandaremus pariatur ita velit
-arbitror se transferrem. Aut fabulas ad aliquip, do quorum eu enim ne aut ut
-minim nostrud, dolor nescius an exercitation, litteris nulla aut quamquam
-exquisitaque, esse cupidatat an ullamco si ullamco hic quem admodum, ubi tamen
-summis an probant.</p>
+<A href="../www/newstudentform.php">New Student</a>
+<A href="">Profiles List</a>
+
+
+
+
 
 <?php
 include('../includes/footer.html');
