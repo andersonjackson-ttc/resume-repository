b57648b03eb83015bbad40cd1ad0582fdfcb1a3c
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-06 23:04:28 -0400

    sqliconnect

--- WS/src/test.php
+++ WS/src/test.php
@@ -8,8 +8,10 @@ $user = "root";
 $password = "rsdbTtc1!";
 $dbName = "resume_schema";
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-or die ('Could not connect to the database server' . mysqli_connect_error());
+$con = mysqli_connect($host, $user, $password, $dbname, $port, $socket);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
 //$con->close();
 
