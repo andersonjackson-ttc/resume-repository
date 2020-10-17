013117b88a653e266d60f6228290b01115f1fa0f
NicholasAJustus <58754949+NicholasAJustus@users.noreply.github.com>
2020-10-17 02:48:00 -0400

    HTML Cosmetics
    Reformatted how the page looked, cleaned up/fixed HTML (Some <div> closers were missing), added a legend, red asterisks, renamed Attachments, removed required attribute from studentID. Currently no media query

--- WS/www/newstudentform.php
+++ WS/www/newstudentform.php
@@ -46,7 +46,6 @@
 		<h1>Create a New Student Form</h1>
 		<form method="post" action="newstudentform.php">
 			<fieldset>
-				<!--Begin first row-->
 				<div id="inputField">       <!--The ' value ' attributes below in PHP will keep whatever you entered in place if the page gets reloaded or you submit an incomplete form.-->
 					<label>Student ID <span class="requiredField">*</span><br><input name="studentID" type="number" size="30" maxlength="100" value="<?php if (isset($_POST['studentID'])) echo $_POST['studentID']; ?>"></label>
 				</div>                      <!--Google ignores/overrides ' autocomplete="off" '. Imagine lots of names will be added, flooding the autofill results. This could be an issue-->
@@ -65,47 +64,52 @@
 				<div id="inputField">
 					<label>Phone Number <span class="requiredField">*</span><br><input required name="studentPhone" type="phone" size="30" maxlength="100" value="<?php if (isset($_POST['studentPhone'])) echo $_POST['studentPhone']; ?>"></label>
 				</div>
-				<div id="checkbox" style="margin-top: 15px;">
-                  			  <label>Military Veteran <span class="requiredField">*</span><br></label>
-                   			  <label for="militaryStatus">Yes<input name="militaryStatus" type="radio" value="yes"></label>
-                  			  <label for="militaryStatus">No<input name="militaryStatus" type="radio" value="no"></label>
-					  <br>
-                			</div>
-                			<div id="checkbox" style="margin-top: 15px;">
-                    				<label>Security Clearance <span class="requiredField">*</span><br></label>
-                   			 	<label for="securityClearance">Yes<input id="securityClearanceYes" name="securityClearance" type="radio" value="yes"></label>
-                    				<label for="securityClearance">No<input id="securityClearanceNo" name="securityClearance" type="radio" value="no"></label>
-                    			<div id="securityAttributes" style="display: none; margin-top: 5px;">
-                        			<label>Select One:
-                            				<label for="securityAttributes">Secret<input name="securityAttributes" type="radio" value="secret"></label>
-                            				<label for="securityAttributes">Top-Secret<input name="securityAttributes" type="radio" value="top-secret"></label>
-                            				<label for="securityAttributes">Confidential<input name="securityAttributes" type="radio" value="confidential"></label>
-                        			</label>
-                        			<label><br>Currently Active?:
-                            				<label for="securityCurrent">Yes<input name="securityCurrent" type="radio" value="yes"></label>
-                            				<label for="securityCurrent">No<input name="securityCurrent" type="radio" value="no"></label>
-                        			</label>
-                    			</div>
+				<hr>
+				<div id="inputField" class="choiceList">
+					<ul style="list-style-type: none;">
+						<h3>General</h3>
+						<div id="checkbox" style="margin-top: 15px;">
+		  			  <label>Military Veteran <span class="requiredField">*</span><br></label>
+		   			  <label for="militaryStatus">Yes<input name="militaryStatus" type="radio" value="yes"></label>
+		  			  <label for="militaryStatus">No<input name="militaryStatus" type="radio" value="no"></label>
+						  <br>
+		  			</div>
+		  			<div id="checkbox" style="margin-top: 15px;">
+							<label>Security Clearance <span class="requiredField">*</span><br></label>
+		   			 	<label for="securityClearance">Yes<input id="securityClearanceYes" name="securityClearance" type="radio" value="yes"></label>
+		  				<label for="securityClearance">No<input id="securityClearanceNo" name="securityClearance" type="radio" value="no"></label>
+		    			<div id="securityAttributes" style="display: none; margin-top: 5px;">
+		      			<label>Select One:
+			    				<label for="securityAttributes">Secret<input name="securityAttributes" type="radio" value="secret"></label>
+			            <label for="securityAttributes">Top-Secret<input name="securityAttributes" type="radio" value="top-secret"></label>
+			    				<label for="securityAttributes">Confidential<input name="securityAttributes" type="radio" value="confidential"></label>
+		      			</label>
+		      			<label><br>Currently Active?:
+			    				<label for="securityCurrent">Yes<input name="securityCurrent" type="radio" value="yes"></label>
+			    				<label for="securityCurrent">No<input name="securityCurrent" type="radio" value="no"></label>
+		      			</label>
+		    			</div>
+						</div>
+						<div id="radio" style="margin-top: 15px;">
+							<label>Work Hours <span class="requiredField">*</span><br></label>
+							<label>Full-Time <input name="workHours" type="radio" value="fullTime"></label>
+							<label> Part-Time <input name="workHours" type="radio" value="partTime"></label>
+						</div>
+						<div id="radio" style="margin-top: 15px;">
+							<label>Work Time <span class="requiredField">*</span><br></label>
+							<label>Days <input name="workTime" type="radio" value="days"></label>
+							<label>Nights <input name="workTime" type="radio" value="nights"></label>
+						</div>
+					</ul>
 				</div>
-				<div id="radio" style="margin-top: 15px;">
-					<label>Work Hours <span class="requiredField">*</span><br></label>
-					<label>Full-Time <input name="workHours" type="radio" value="fullTime"></label>
-					<label> Part-Time <input name="workHours" type="radio" value="partTime"></label>
-				</div>
-				<div id="radio" style="margin-top: 15px;">
-					<label>Work Time <span class="requiredField">*</span><br></label>
-					<label>Days <input name="workTime" type="radio" value="days"></label>
-					<label>Nights <input name="workTime" type="radio" value="nights"></label>
-				</div>
-				<!--End first row-->
-				<br>
-				<!--Begin second row-->
-				<div id="inputField">
+
+				<div id="inputField" class="choiceList">
 					<!-- Start of Technical Skills Checkboxes -->
-					<div id="techSkills" style="display: inline-block;">
+					<div id="techSkills">
 						<ul style="list-style-type: none;">
 							<h3>Technical Skills</h3>
 							<li>
+								<br>
 								<input type="checkbox" id="skillProgram" name="skillProgram" value="skillProgram">
 								<label for="skillProgram">Programming Languages (1 or more)</label>
 							</li>
@@ -142,7 +146,7 @@
 				</div>
 				<div id="inputField">
 					<!-- Start of Professional Skills Ratings -->
-					<div id="profSkillsRating" style="display: inline-block; margin-left: 20px;">
+					<div id="profSkillsRating">
 						<ul style="list-style-type: none;">
 							<h3>Professional Skills</h3>
 							<li style="column-count: 2;">
@@ -289,45 +293,65 @@
 							</li>
 							<li style="column-count: 2;">
 								<div>
-									<label><br>Troubleshooting</label>
+									<label>Troubleshooting</label>
 								</div>
-								<div>
-									<label for="skillTroubleshootingRating"><text>Fair</text><text style="margin-left: 15px;">Good</text><text style="margin-left: 15px;">Excellent</text><br>
-										<input type="radio" id="skillTroubleshootingRating" name="skillTroubleshootingRating" value="fair">
-										<input type="radio" id="skillTroubleshootingRating" name="skillTroubleshootingRating" value="good" style="margin-left: 30px;">
-										<input type="radio" id="skillTroubleshootingRating" name="skillTroubleshootingRating" value="excellent" style="margin-left: 40px;">
-									</label>
-								</div>
+									<input type="radio" id="skillTroubleshootingRating" name="skillTroubleshootingRating" value="fair">
+									<input type="radio" id="skillTroubleshootingRating" name="skillTroubleshootingRating" value="good" style="margin-left: 30px;">
+									<input type="radio" id="skillTroubleshootingRating" name="skillTroubleshootingRating" value="excellent" style="margin-left: 40px;">
 							</li>
 						</ul>
 					</div> <!-- End of Professional Skills Ratings -->
 				</div>
-				<!--End second row-->
-				<br>
-				<!--Begin third row-->
-				<div id="inputField">
-					<label>Experience<br></label>
+				<div id="inputField" class="choiceList">
+					<!-- Job Interest Checkboxes -->
+					<div id="jobInterest">
+						<ul style="list-style-type: none;">
+								<h3>Job Interests</h3>
+								<li>
+										<input type="checkbox" id="itDirector" name="itDirector" value="itDirector">
+										<label for="itDirector">IT Director</label>
+								</li>
+								<li>
+										<input type="checkbox" id="dataAnalyst" name="dataAnalyst" value="dataAnalyst">
+										<label for="dataAnalyst">Data Analyst</label>
+								</li>
+								<li>
+										<input type="checkbox" id="cyberSecurity" name="cyberSecurity" value="cyberSecurity">
+										<label for="cyberSecurity">Cyber Security</label>
+								</li>
+								<li>
+										<input type="checkbox" id="applicationsEngineer" name="applicationsEngineer" value="applicationsEngineer">
+										<label for="applicationsEngineer">Applications Engineer</label>
+								</li>
+						</ul>
+						<ul style="list-style-type: none;">
+									<li>
+											<input type="checkbox" id="supportSpecialist" name="supportSpecialist" value="supportSpecialist">
+											<label for="supportSpecialist">Support Specialist</label>
+									</li>
+									<li>
+											<input type="checkbox" id="networkEngineer" name="networkEngineer" value="networkEngineer">
+											<label for="networkEngineer">Network Engineer</label>
+									</li>
+									<li>
+											<input type="checkbox" id="webDeveloper" name="webDeveloper" value="webDeveloper">
+											<label for="webDeveloper">Web Developer</label>
+									</li>
+									<li>
+											<input type="checkbox" id="dataAdministrator" name="dataAdministrator" value="dataAdministrator">
+											<label for="dataAdministrator">Database Administrator</label>
+									</li>
+						</ul>
+					</div><!-- End of Job Interest Checkboxes -->
 				</div>
-				<!--End third row-->
-				<br>
-				<!--Begin fourth row-->
-				<div id="inputField">
-					<label>Education<br></label>
-					<label>Select One:<br>
-                            			<label for="securityAttributes">Associates<input name="securityAttributes" type="radio" value="associates"></label>
-                            			<label for="securityAttributes">Bachelors<input name="securityAttributes" type="radio" value="bachelors"></label>
-                            			<label for="securityAttributes">Masters<input name="securityAttributes" type="radio" value="masters"></label>
-						<label for="securityAttributes">Doctorate<input name="securityAttributes" type="radio" value="doctorate"></label>
-						<label for="securityAttributes">PHD<input name="securityAttributes" type="radio" value="phd"></label>
-                        			<br>
-                       				<label for="collegeName">College: <br><input name="collegeName" type="text" value="<?php if (isset($_POST['collegeName'])) echo $_POST['collegeName']; ?>" style="margin-top: 5px;"></label>
-                        			<br>
-                        			<label for="degreeName">Degree: <br><input name="degreeName" type="text" value="<?php if (isset($_POST['degreeName'])) echo $_POST['degreeName']; ?>" style="margin-left: 4px; margin-top: 5px;"></label>
-                        		</label>
-					<br>
+				<hr>
+				<div id="inputField"> <!--Do we still want this field? leaving it as a text box for now-->
+					<label>Experience<br><textarea class="longBar" name="experience" type="text"><?php if (isset($_POST['experience'])) echo $_POST['experience']; ?></textarea></label>
 				</div>
-				<div id="inputField" class="gradDiv">
+				<br>
+				<div id="inputField" class="eduList">
 					<div>
+						<label><h3>Graduation</h3></label>
 						<label for="gradStatus">Graduation Status <span class="requiredField">*</span><br></label>
 						<select required name="gradStatus" id="gradStatus" class="gradFields">
 							<option disabled selected value="">-- select an option --</option>
@@ -338,72 +362,44 @@
 					<div>
 						<label>Graduation Date <span class="requiredField">*</span><br><input class="gradFields" required name="gradDate" type="date" value="<?php if (isset($_POST['gradDate'])) echo $_POST['gradDate']; ?>"></label>
 					</div>
-					<div id="inputField">
-						<!-- Job Interest Checkboxes -->
-						<div id="jobInterest">
-							<ul style="list-style-type: none; display: inline-block;">
-									<h3>Job Interests</h3>
-									<li>
-											<input type="checkbox" id="itDirector" name="itDirector" value="itDirector">
-											<label for="itDirector">IT Director</label>
-									</li>
-									<li>
-											<input type="checkbox" id="dataAnalyst" name="dataAnalyst" value="dataAnalyst">
-											<label for="dataAnalyst">Data Analyst</label>
-									</li>
-									<li>
-											<input type="checkbox" id="cyberSecurity" name="cyberSecurity" value="cyberSecurity">
-											<label for="cyberSecurity">Cyber Security</label>
-									</li>
-									<li>
-											<input type="checkbox" id="applicationsEngineer" name="applicationsEngineer" value="applicationsEngineer">
-											<label for="applicationsEngineer">Applications Engineer</label>
-									</li>
-							</ul>
-							<ul style="list-style-type: none;  display:inline-block;">
-										<li>
-												<input type="checkbox" id="supportSpecialist" name="supportSpecialist" value="supportSpecialist">
-												<label for="supportSpecialist">Support Specialist</label>
-										</li>
-										<li>
-												<input type="checkbox" id="networkEngineer" name="networkEngineer" value="networkEngineer">
-												<label for="networkEngineer">Network Engineer</label>
-										</li>
-										<li>
-												<input type="checkbox" id="webDeveloper" name="webDeveloper" value="webDeveloper">
-												<label for="webDeveloper">Web Developer</label>
-										</li>
-										<li>
-												<input type="checkbox" id="dataAdministrator" name="dataAdministrator" value="dataAdministrator">
-												<label for="dataAdministrator">Database Administrator</label>
-										</li>
-							</ul>
-						</div><!-- End of Job Interest Checkboxes -->
 				</div>
-				<!--End fourth row-->
-				<br>
-				<!--Begin fifth row-->
+				<div id="inputField" class="eduList">
+					<label><h3>Education</h3></label>
+					<label>Select One:<br>
+      			<label for="securityAttributes">Associates<input name="securityAttributes" type="radio" value="associates"></label>
+      			<label for="securityAttributes">Bachelors<input name="securityAttributes" type="radio" value="bachelors"></label>
+      			<label for="securityAttributes">Masters<input name="securityAttributes" type="radio" value="masters"></label>
+						<label for="securityAttributes">Doctorate<input name="securityAttributes" type="radio" value="doctorate"></label>
+						<label for="securityAttributes">PHD<input name="securityAttributes" type="radio" value="phd"></label>
+	    			<br>
+	   				<label for="collegeName">College: <br><input name="collegeName" type="text" value="<?php if (isset($_POST['collegeName'])) echo $_POST['collegeName']; ?>" style="margin-top: 5px;"></label>
+	    			<br>
+	    			<label for="degreeName">Degree: <br><input name="degreeName" type="text" value="<?php if (isset($_POST['degreeName'])) echo $_POST['degreeName']; ?>" style="margin-left: 4px; margin-top: 5px;"></label>
+	    		</label>
+					<br>
+				</div>
+				<div id="inputField" class="eduList">
+					<label><h3>Majors <span class="requiredField">*</span></h3><br></label>
+					<div>
+						<p>lorem ipsum</p>
+					</div>
+				</div>
+				<div id="inputField" class="eduList">
+					<label><h3>Certifications</h3><br></label>
+					<div>
+						<p>lorem ipsum</p>
+					</div>
+				</div>
 				<div id="inputField">
-					<label>Majors <span class="requiredField">*</span><br></label>
+					<label>Upload Resume <span class="requiredField">*</span><br><input required name="attachments" type="file"></label>
 				</div>
-				<!--End fifth row-->
-				<br>
-				<!--Begin sixth row-->
-				<div id="inputField">
-					<label>Certifications<br></label>
-				</div>
-				<!--End sixth row-->
-				<br>
-				<!--Begin seventh row-->
-				<div id="inputField">
-					<label>Attachments <span class="requiredField">*</span><br><input required name="attachments" type="file"></label>
-				</div>
-				<!--End seventh row-->
 				<!--Submit button and Exit button-->
 				<br>
 				<br>
+
 				<input id="finalButton" type="submit" value="Save Form" style="float: left;">
 				<a href="index.php"><input id="finalButton" type="button" value="Cancel and Exit" style="float: left;"></a>
+				<div style="float: right;"><span class="requiredField">*</span> = Required Field</div>
 			</fieldset>
 		</form>
 		<?php
