edb0b8b4d63d03959ba8f594ddf2f2d2e192073b
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-22 16:53:54 -0400

    names

--- TS/www/test_studentform.php
+++ TS/www/test_studentform.php
@@ -14,9 +14,9 @@
 		<form name="test_studentform.php" method="POST" action="test_student_submit.php">
 		
 		
-		<label>First Name *<br><input required name="first" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></label>
+		<label>First Name *<br><input required name="firstName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></label>
 		<br>
-		<label>Last Name *<br><input required name="first" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></label>
+		<label>Last Name *<br><input required name="lastName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></label>
 
 		
 		
