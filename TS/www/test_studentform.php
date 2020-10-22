46d63e507cd17ee2c6a38d07b01f5017e526aec3
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-22 13:40:20 -0400

    Update test_studentform.php

--- TS/www/test_studentform.php
+++ TS/www/test_studentform.php
@@ -14,23 +14,17 @@
 
 		<h1>Create a New Student Form</h1>
 		<form name="test_studentform.php" method="POST" action="test_student_submit.php" >
-			<fieldset>
-				<div id="inputField">       <!--The ' value ' attributes below in PHP will keep whatever you entered in place if the page gets reloaded or you submit an incomplete form.-->
-					<label>Student ID <span class="requiredField">*</span><br><input name="studentID" type="number" size="30" maxlength="100" value="<?php if (isset($_POST['studentID'])) echo $_POST['studentID']; ?>"></label>
-				</div>                      <!--Google ignores/overrides ' autocomplete="off" '. Imagine lots of names will be added, flooding the autofill results. This could be an issue-->
-				<div id="inputField">       <!--The ' required ' attribute makes the field unable to submit unless it is filled out.-->
-					<label>First Name <span class="requiredField">*</span><br><input required name="firstName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></label>
-				</div>
-				<div id="inputField">
-					<label>MI<br><input name="middleInitial" type="text" size="5" maxlength="100" value="<?php if (isset($_POST['middleInitial'])) echo $_POST['middleInitial']; ?>"></label>
-				</div>
-				<div id="inputField">
-					<label>Last Name <span class="requiredField">*</span><br><input required name="lastName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></label>
-				</div>
-				
+		
+		
+		<label>Student ID *<br><input required name="first" type="number" size="30" maxlength="100" value="<?php if (isset($_POST['studentID'])) echo $_POST['studentID']; ?>"></label>
+		<label>First Name *<br><input required name="first" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></label>
+		<label>Middle Initial *<br><input required name="first" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['middleInitial'])) echo $_POST['middleInitial']; ?>"></label>
+		<label>Last Name *<br><input required name="first" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></label>
 
-				<hr><br>
-
+		
+		
+		
+			
 				<!--Submit button and Exit button-->
 				<br>
 				<br>
@@ -38,7 +32,7 @@
 				<button type="submit" name="submit" style="float: left;">Submit</button>
 				<a href="index.php"><input type="button" value="Cancel and Exit" style="float: left;"></a>
 				<div style="float: right;"><span class="requiredField">*</span> = Required Field</div>
-			</fieldset>
+			
 		</form>
 
 		<?php
