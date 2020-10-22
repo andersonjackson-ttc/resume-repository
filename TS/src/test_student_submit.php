248a4528befd0fab9c5a20867db7d30473b616a4
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-22 12:33:54 -0400

    attributes

--- TS/src/test_student_submit.php
+++ TS/src/test_student_submit.php
@@ -8,11 +8,7 @@
   $lastName = $_POST['lastName'];
   $email = $_POST['studentEmail'];
   $phone = $_POST['studentPhone'];
-  $gradStatus = 1;
-  $gradDate = 'CURDATE()';
-  $resumePath = 'resumePathTest';
-  $milStatus = 1;
-  $clearance = 2;
+ 
 
   $sql = "INSERT INTO students (student_id, first_name, middle_initial, last_name, email, phone)
           VALUES ('$studentId', '$firstName', '$middleInitial', '$lastName', '$email', '$phone');"
