fddf673740fde842c506650c3689e3d4fbd777a7
JBihlear <71603063+JBihlear@users.noreply.github.com>
2020-10-22 20:42:00 -0400

    Fixed syntax errors
    Fixed errors in the SQL call for the student's selected skills.

--- TS/src/student_edit_connection.php
+++ TS/src/student_edit_connection.php
@@ -9,9 +9,7 @@
   $studentResult = mysqli_query($con, $sqlSelectStudent);
 
   #Pull Student Skills using profile ID
-  $sqlSelectStudentSkills = "SELECT student_id, first_name, middle_initial, last_name,
-  email, phone, graduated, graduation_date, military_status, security_clearance
-  FROM student_skills WHERE profile_id = ".profile_id;
+  $sqlSelectStudentSkills = "SELECT skill_id FROM student_skills WHERE profile_id = ".profile_id;
   $studentSkillsResult = mysqli_query($con, $sqlSelectStudentSkills);
 
   #Pull List of Skills from database
