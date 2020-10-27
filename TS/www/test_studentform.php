4b316a53b49784a80426de6d72e5173b5d433a23
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-27 12:13:57 -0400

    javascript reint

--- TS/www/test_studentform.php
+++ TS/www/test_studentform.php
@@ -14,20 +14,60 @@
 		<form name="test_studentform.php" method="POST" action="test_student_submit.php">
 		
 		<label>Student ID <br><input name="studentID" type="number" size="30" maxlength="100" value="<?php if (isset($_POST['studentID'])) echo $_POST['studentID']; ?>"></label>
-		<br>
 		
+		<!--Submits-->
 		<label>First Name *<br><input required name="firstName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></label>
-		<br>
+		<
 		
 		<label>MI<br><input name="middleInitial" type="text" size="5" maxlength="100" value="<?php if (isset($_POST['middleInitial'])) echo $_POST['middleInitial']; ?>"></label>
 
-		
+		<!--Submits-->
 		<label>Last Name *<br><input required name="lastName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></label>
 
 
 		<label>Email <span class="requiredField">*</span><br><input required name="studentEmail" type="email" size="30" maxlength="100" value="<?php if (isset($_POST['studentEmail'])) echo $_POST['studentEmail']; ?>"></label>
 
+		<label>Phone Number <span class="requiredField">*</span><br><input required name="studentPhone" type="phone" size="30" maxlength="100" value="<?php if (isset($_POST['studentPhone'])) echo $_POST['studentPhone']; ?>"></label>
+
 		
+		
+		<div id="inputField" class="choiceList">
+					<ul style="list-style-type: none;">
+						<h3>General</h3>
+						<div id="checkbox" style="margin-top: 15px;">
+		  			  <label>Military Veteran <span class="requiredField">*</span><br></label>
+		   			  <label for="militaryStatus">Yes<input name="militaryStatus" type="radio" value="yes"></label>
+		  			  <label for="militaryStatus">No<input name="militaryStatus" type="radio" value="no"></label>
+						  <br>
+		  			</div>
+		  			<div id="checkbox" style="margin-top: 15px;">
+							<label>Security Clearance <span class="requiredField">*</span><br></label>
+		   			 	<label for="securityClearance">Yes<input id="securityClearanceYes" name="securityClearance" type="radio" value="yes"></label>
+		  				<label for="securityClearance">No<input id="securityClearanceNo" name="securityClearance" type="radio" value="no"></label>
+		    			<div id="securityAttributes" style="display: none; margin-top: 5px;">
+		      			<label>Select One:
+			    				<label for="securityAttributes">Secret<input name="securityAttributes" type="radio" value="secret"></label>
+			            <label for="securityAttributes">Top-Secret<input name="securityAttributes" type="radio" value="top-secret"></label>
+			    				<label for="securityAttributes">Confidential<input name="securityAttributes" type="radio" value="confidential"></label>
+		      			</label>
+		      			<label><br>Currently Active?:
+			    				<label for="securityCurrent">Yes<input name="securityCurrent" type="radio" value="yes"></label>
+			    				<label for="securityCurrent">No<input name="securityCurrent" type="radio" value="no"></label>
+		      			</label>
+		    			</div>
+						</div>
+						<div id="radio" style="margin-top: 15px;">
+							<label>Work Hours <span class="requiredField">*</span><br></label>
+							<label>Full-Time <input name="workHours" type="radio" value="fullTime"></label>
+							<label> Part-Time <input name="workHours" type="radio" value="partTime"></label>
+						</div>
+						<div id="radio" style="margin-top: 15px;">
+							<label>Work Time <span class="requiredField">*</span><br></label>
+							<label>Days <input name="workTime" type="radio" value="days"></label>
+							<label>Nights <input name="workTime" type="radio" value="nights"></label>
+						</div>
+					</ul>
+				</div>
 		
 		
 			
