4865ef084f3c43d879ffb75a49a03c2d3c466279
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-27 10:43:25 -0400

    students_test to students try

--- TS/www/test_student_submit.php
+++ TS/www/test_student_submit.php
@@ -1,12 +1,23 @@
 <?php
   include_once '../src/connection.php';
 
+  $studentId = 1;
   
   $firstName = $_POST['firstName'];
+  $middleInitial = 'a';
   $lastName = $_POST['lastName'];
+  
+  $email = 'name@email.com';
+  $phone = '1234567890';
+  $gradStatus = 1;
+  $gradDate = 'CURDATE()';
+  $resumePath = 'path';
+  $milStatus = 1;
+  $clearance = 2;
 
 
-  $sql = "INSERT INTO students_test (first_name, last_name) VALUES ('$firstName', '$lastName');";
+  $sql = "INSERT INTO students (student_id, first_name, middle_initial, last_name, email, phone, graduated, graduation_date, resume_path, military_status, security_clearence) 
+								VALUES ('$studentId', '$firstName', '$middleInitial', '$lastName', '$email', '$phone', '$gradStatus', '$gradDate','$resumePath', '$milStatus', '$clearance');";
 
 
 
