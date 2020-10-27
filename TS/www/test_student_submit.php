c3f684b26f29463f72fd2e18f798aac42cd632ba
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-27 11:16:11 -0400

    Update test_student_submit.php

--- TS/www/test_student_submit.php
+++ TS/www/test_student_submit.php
@@ -23,11 +23,11 @@
 if ($con->query($sql) === TRUE) {
   header("Location: ../test_studentform.php?name_add=success");
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . $con->error;
 }
 
 
-$conn->close();
+$con->close();
 ?>
 
 
