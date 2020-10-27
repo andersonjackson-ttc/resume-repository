1d1972b0172ce06e4e7c890b136f5968a7ec5fee
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-10-27 12:00:08 -0400

    Update test_studentform.php

--- TS/www/test_studentform.php
+++ TS/www/test_studentform.php
@@ -13,10 +13,19 @@
 		<h1>Create a New Student Form</h1>
 		<form name="test_studentform.php" method="POST" action="test_student_submit.php">
 		
+		<label>Student ID <br><input name="studentID" type="number" size="30" maxlength="100" value="<?php if (isset($_POST['studentID'])) echo $_POST['studentID']; ?>"></label>
+		<br>
 		
 		<label>First Name *<br><input required name="firstName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></label>
 		<br>
+		
+		<label>MI<br><input name="middleInitial" type="text" size="5" maxlength="100" value="<?php if (isset($_POST['middleInitial'])) echo $_POST['middleInitial']; ?>"></label>
+
+		
 		<label>Last Name *<br><input required name="lastName" type="text" size="30" maxlength="100" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></label>
+
+
+		<label>Email <span class="requiredField">*</span><br><input required name="studentEmail" type="email" size="30" maxlength="100" value="<?php if (isset($_POST['studentEmail'])) echo $_POST['studentEmail']; ?>"></label>
 
 		
 		
