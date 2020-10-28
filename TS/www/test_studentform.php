63e4f66a0422bd9f50d36d7fd47dc7eb9e108f1a
Dustin W <56456240+dustinwhite009@users.noreply.github.com>
2020-10-27 23:24:43 -0400

    Update test_studentform.php

--- TS/www/test_studentform.php
+++ TS/www/test_studentform.php
@@ -9,9 +9,10 @@
 		?>
 		
 		
+
+		<form name="test_studentform.php" method="POST" action="test_student_submit.php">
 		<fieldset>
 		<legend>Create a New Student Form</legend>
-		<form name="test_studentform.php" method="POST" action="test_student_submit.php">
 		
 		<label>Student ID <br><input name="studentID" type="number" size="30" maxlength="100" value="<?php if (isset($_POST['studentID'])) echo $_POST['studentID']; ?>"></label>
 		
