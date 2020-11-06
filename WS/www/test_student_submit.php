0893e439c507e20cd3555c0c4fd9dfec622d6b3f
joshuaashy <57810408+joshuaashy@users.noreply.github.com>
2020-11-06 12:02:41 -0500

    working submission
    submits everything but:
      $gradStatus = 3;
      $gradDate = 1; 
      $resumePath

--- WS/www/test_student_submit.php
+++ WS/www/test_student_submit.php
@@ -1,34 +1,115 @@
 <?php
-  include_once '../src/connection.php';
+$page_title = 'Submit';
 
-  $studentId = 1;
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+include_once '../src/connection.php';
+
+$errors = [];
+
+if(empty($_POST['firstName'])){
+	$errors[] = 'Required field:';
+	}else{
+		$firstName =  trim($_POST['firstName']);
+	}
+if(empty($_POST['middleInitial'])){
+	$errors[] = 'Required field:';
+	}else{
+		$middleInitial = trim($_POST['middleInitial']);
+	}
+
+if(empty($_POST['lastName'])){
+	$errors[] = 'Required field:';
+	}else{
+	  $lastName = trim($_POST['lastName']);
+	}  
+if(empty($_POST['studentId'])){
+	$errors[] = 'Required field:';
+	}else{
+	 $studentId = trim($_POST['studentId']);
+	} 
+if(empty($_POST['email'])){
+	$errors[] = 'Required field:';
+	}else{
+	 $email = trim($_POST['email']);
+	} 
+if(empty($_POST['phone'])){
+	$errors[] = 'Required field:';
+	}else{
+	$phone = trim($_POST['phone']);
+	} 
+ if(empty($_POST['militaryStatus'])){
+	$errors[] = 'Required field:';
+	}else{
+	$milStatus = trim($_POST['militaryStatus']);
+	} 
+ if(empty($_POST['securityClearance'])){
+	$errors[] = 'Required field:';
+	}else{
+	$clearance = trim($_POST['securityClearance']);
+	} 
+ 
+ 
   
-  $firstName = $_POST['firstName'];
-  $middleInitial = 'a';
-  $lastName = $_POST['lastName'];
-  
-  $email = 'name@email.com';
-  $phone = '1234567890';
-  $gradStatus = 1;
-  $gradDate = '1'; //not in SQL statment
+  $gradStatus = 3;
+  $gradDate = 1; //not in SQL statment
   $resumePath = 'path';
-  $milStatus = 1;
-  $clearance = 2;
 
 
-  $sql = "INSERT INTO students (student_id, first_name, middle_initial, last_name, email, phone, graduated, resume_path, military_status, security_clearance) VALUES ($studentId , '$firstName', '$middleInitial', '$lastName', '$email', '$phone', $gradStatus,'$resumePath', $milStatus, $clearance);";
 
-	//$q = "SELECT COUNT(certificates) FROM resume_schema";
+$sql = "INSERT INTO `students`( `student_id`, `first_name`, `middle_initial`, `last_name`, `email`, `phone`, `graduated`, `graduation_date`, `resume_path`, `military_status`, `security_clearance`) VALUES (SHA2('$studentId', 512),'$firstName', '$middleInitial', '$lastName', '$email', '$phone', $gradStatus,'$gradDate','$resumePath', '$milStatus', '$clearance' )";
+//$q = "SELECT COUNT(certificates) FROM resume_schema";
 
+
+$checkbox1=$_POST['skills'];  
+  $N = count($checkbox1);
+
+   
+$chk="";  
+	foreach($checkbox1 as $chk1)  
+		 {  
+		 $chk .= $chk1.",";  
+		 }  
+		$in_ch=mysqli_query($con,"insert into skills (skills) values ('$chk')"); 
+
+		
+$checkbox2=$_POST['job_interest'];  
+   $J = count($checkbox2);
+
+   
+	$chkj="";  
+	foreach($checkbox2 as $chk2)  
+	{  
+	$chkj .= $chk2.",";  
+	}  
+	$in_ch=mysqli_query($con,"insert into job_interest (job_interest) values ('$chkj')"); 
+		
+$checkbox3=$_POST['certification'];  
+   $C = count($checkbox3);
+
+   
+$chkc="";  
+	foreach($checkbox3 as $chk3)  
+	{  
+	$chkc .= $chk3.",";  
+	}  
+	$in_ch=mysqli_query($con,"insert into certification (certification) values ('$chkc')"); 
 
 if ($con->query($sql) === TRUE) {
-  header("Location: ../test_studentform.php?name_add=success");
+  header("Location: ../www/test_studentform.php?name_add=success");
 } else {
   echo "Error: " . $sql . "<br>" . $con->error;
 }
-
-
 $con->close();
+
+}	
+
+	
+
+
+
+
 ?>
 
 
