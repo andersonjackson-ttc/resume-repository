7b207092c47f011227bc96852c12a1f8008d7800
NicholasAJustus <58754949+NicholasAJustus@users.noreply.github.com>
2020-11-09 16:50:03 -0500

    Reverting Filter Support
    Reverting my current filter support just to have the most stable version available. I will be moving to the login page so my work on the filter will be halted for now. If you want to work on the filter feel free, as my previous version did not support joint tables yet. This version still has functional keyword and exact search support.

--- WS/www/search_home.php
+++ WS/www/search_home.php
@@ -1,4 +1,4 @@
-<!--Nicholas Justus 11/06/20: This is a basic page that just includes the search bar and filter options-->
+<!--Nicholas Justus 11/06/20: This is a basic page that just includes the search bar-->
 <?php
   $page_title = 'Student Search';
   include ('../includes/header.html');
@@ -9,14 +9,6 @@
 <form action="search_results.php" method="POST" style="display:flex;justify-content:center;">
   <input type="text" name="search" placeholder="Search">
   <button type="submit" name="submit-search">Go</button>
-
-  <input type="checkbox" id="filterGraduates" name="filterGraduates" value="Graduates">
-  <label for="filterGraduates"> Graduates</label><br>
-
-  <input type="checkbox" id="filterMilitary" name="filterMilitary" value="Military">
-  <label for="filterMilitary"> Military</label><br>
-
-  </select>
 </form>
 <p style="display:flex;justify-content:center;font-style:italic;opacity:0.4;">Hint: Place ! before a search for exact results.</p>
 </body>
