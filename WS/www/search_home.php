d15769c6002c788d601a1d9461d6a57dae9934c9
NicholasAJustus <58754949+NicholasAJustus@users.noreply.github.com>
2020-11-09 00:01:39 -0500

    Added Search Filtering capabilities
    You can now filter your search results (Currently only supports graduates and military, but should be easily extendable) and any filtered condition will be written to the query

--- WS/www/search_home.php
+++ WS/www/search_home.php
@@ -1,4 +1,4 @@
-<!--Nicholas Justus 11/06/20: This is a basic page that just includes the search bar-->
+<!--Nicholas Justus 11/06/20: This is a basic page that just includes the search bar and filter options-->
 <?php
   $page_title = 'Student Search';
   include ('../includes/header.html');
@@ -9,6 +9,14 @@
 <form action="search_results.php" method="POST" style="display:flex;justify-content:center;">
   <input type="text" name="search" placeholder="Search">
   <button type="submit" name="submit-search">Go</button>
+
+  <input type="checkbox" id="filterGraduates" name="filterGraduates" value="Graduates">
+  <label for="filterGraduates"> Graduates</label><br>
+
+  <input type="checkbox" id="filterMilitary" name="filterMilitary" value="Military">
+  <label for="filterMilitary"> Military</label><br>
+
+  </select>
 </form>
 
 </body>
