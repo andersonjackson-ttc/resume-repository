5bd935e366375a523eee48be2ce1fdde403c5123
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-11-10 21:11:57 -0500

    show_students.php

--- old_WS/www/newstudentform.php
+++ old_WS/www/newstudentform.php
@@ -516,8 +516,6 @@
 				<div style="float: right;"><span class="requiredField">*</span> = Required Field</div>
 			</fieldset>
 		</form>
-
-		 <script src="../includes/new_student_regex.js" type="text/javascript"></script>
 		<?php
 		include ('../includes/footer.html');
 		?>
