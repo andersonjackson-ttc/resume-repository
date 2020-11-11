7068828204428ad5666bee0dfc8a9a128d509535
NicholasAJustus <58754949+NicholasAJustus@users.noreply.github.com>
2020-11-10 22:29:22 -0500

    Added Login Functions
    A file to be included for the login system. Verifies logins and redirects user

---
+++ WS/includes/login_functions.php
@@ -0,0 +1,43 @@
+<?php
+//Redirect user to absolute URL
+function redirect_user($page = 'index.php') {
+  //Create URL definition
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+	//Sprucing up the URL
+	$url = rtrim($url, '/\\');
+	$url .= '/' . $page;
+	//Redirect to url value
+	header("Location: $url");
+	exit();
+}
+
+function check_login($con, $user_email = '', $user_password = '') {
+
+	$errors = [];
+	//Email
+	if (empty($user_email)) {
+		$errors[] = 'No email has been entered.';
+	} else {
+		$e = mysqli_real_escape_string($con, trim($user_email));
+	}
+	//Password
+	if (empty($user_password)) {
+		$errors[] = 'No password has been entered.';
+	} else {
+		$p = mysqli_real_escape_string($con, trim($user_password));
+	}
+
+	if (empty($errors)) {
+		//Find the user that matches input information
+		$query = "SELECT user_id FROM users WHERE user_email='$e' AND user_password=SHA1('$p')";
+		$result = @mysqli_query($con, $query);
+
+		if (mysqli_num_rows($result) == 1) {
+			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			return [true, $row];
+		} else {
+			$errors[] = 'You have entered an invalid email or password.';
+		}
+	}
+	return [false, $errors];
+}
