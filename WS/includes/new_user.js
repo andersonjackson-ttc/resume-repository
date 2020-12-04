a0c34af94c6aab672581b094dfd7d0459b33903e
khalidsmalls <53487854+khalidsmalls@users.noreply.github.com>
2020-12-04 00:34:34 -0500

    Update new_user.js

--- WS/includes/new_user.js
+++ WS/includes/new_user.js
@@ -7,29 +7,29 @@ $(document) .ready(function() {
       $email_err = $('#email_err');
 
       $('.err_span') .hide();
-      $('.required_field') .addClass('text-danger');
-
-      $first_name .on('blur', function() {
-        if ($first_name.val() == '') {
-          $first_name_err .slideDown();
-        }
-      }) .on('focus', function() {
-        $first_name_err .slideUp();
-      });
-
-      $last_name .on('blur', function() {
-        if ($last_name.val() == '') {
-          $last_name_err .slideDown();
-        }
-      }) .on('focus', function() {
-        $last_name_err .slideUp();
-      });
-
-      $email .on('blur', function() {
-        if ($email.val() == '') {
-          $email_err .slideDown();
-        }
-      }) .on('focus', function() {
-        $email_err .slideUp();
-      });
+      // $('.required_field') .addClass('text-danger');
+      //
+      // $first_name .on('blur', function() {
+      //   if ($first_name.val() == '') {
+      //     $first_name_err .slideDown();
+      //   }
+      // }) .on('focus', function() {
+      //   $first_name_err .slideUp();
+      // });
+      //
+      // $last_name .on('blur', function() {
+      //   if ($last_name.val() == '') {
+      //     $last_name_err .slideDown();
+      //   }
+      // }) .on('focus', function() {
+      //   $last_name_err .slideUp();
+      // });
+      //
+      // $email .on('blur', function() {
+      //   if ($email.val() == '') {
+      //     $email_err .slideDown();
+      //   }
+      // }) .on('focus', function() {
+      //   $email_err .slideUp();
+      // });
 });
